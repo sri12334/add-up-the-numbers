99c47fe9564111b33074f35cca1837c9c9620767
sri12334 <srir48747@gmail.com>
2024-04-09 15:52:49 +0200

    did the challange

---
+++ add-up-numbers/solution1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Add up all the numbers from 1 to the given input number.
+ *
+ * @param {number} number - The number up to which the sum needs to be calculated.
+ * @returns {number} The sum of all numbers from 1 to the input number.
+ */
+
+const addUp = (number) => {
+    let sum = 0;
+    for (let i = 1; i <= number; i++) {
+        sum += i;
+    }
+    return sum;
+}
